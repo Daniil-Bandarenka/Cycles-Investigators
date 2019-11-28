b09443cf8170d80be9499af3d57a769aa810f7ca
Daniil Bandarenka1 <Daniil_Bandarenka1@epam.com>
2019-11-28 14:59:51 +0300

    fix 038

--- investigators/038.py
+++ investigators/038.py
@@ -1,19 +1,3 @@
-# from datetime import datetime
-# from lxml import html
-#
-# import requests
-#
-# try:
-#     r = requests.get("http://www.northwest.williams.com/NWP_Portal/CapacityResultsScrollable.action")
-#
-#     html_tree = html.fromstring(r.content)
-#     cycle = html_tree.xpath("//*[*/text()='Cycle:']/following-sibling::td//text()")
-#
-#     print(f"{datetime.now()} : {cycle[0]}")
-# except Exception as e:
-#     print(f"{datetime.now()} : {str(e)}")
-
-
 import os
 from datetime import datetime
 
